575035cd9b9ad3e1a3802963da830ff4ed7e0a10
Victor Frye <victorfrye@outlook.com>
2025-05-15 17:21:47 -0400

    chore: flatten topology + devcontainer

--- src/WebClient/components/theme/useDarkMode.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-
-import { DarkModeContext } from '@shrugman/components/theme/DarkMode';
-
-const useDarkMode = () => useContext(DarkModeContext);
-
-export default useDarkMode;
