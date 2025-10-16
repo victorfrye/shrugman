22609a22adf53ed712f9f722102289ff9c9d7b52
Victor Frye <victorfrye@outlook.com>
2025-10-16 09:46:46 -0400

    chore: clear maintf

--- infra/main.tf
+++ infra/main.tf
@@ -1,10 +1,1 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
 
-resource "azurerm_static_web_app" "example" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-}
